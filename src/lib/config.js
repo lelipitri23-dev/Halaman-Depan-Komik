d9767e343b9686f46702554fadca31b6bf8863bf
xrofed <akunhdhdnsjsj@gmail.com>
2026-02-19 22:39:14 +0700

    fix 404

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,16 +1,12 @@
 // config.js
 export const SITE_CONFIG = {
   // PERBAIKAN DI SINI: Gunakan NEXT_PUBLIC_SITE_NAME
-  name: process.env.NEXT_PUBLIC_SITE_NAME || 'Komikcast',
+  name: process.env.NEXT_PUBLIC_SITE_NAME || 'komikcast',
   
   baseUrl: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000',
-  
-  // SEO & Metadata
-  // Catatan: Jika ini hanya dipakai di Metadata (Server Component), tidak wajib pakai NEXT_PUBLIC_.
-  // Tapi jika error berlanjut, ubah juga menjadi NEXT_PUBLIC_SITE_DESCRIPTION di .env dan disini.
-  description: process.env.SITE_DESCRIPTION || 'Komikcast adalah situs baca komik, baca manga, baca manhua, dan baca manhwa terpopuler dalam Bahasa Indonesia..',
-  keywords: (process.env.SITE_KEYWORDS || 'komikcast, komik online, komikcast to, komiku, manga, manhwa').split(', '),
+  description: process.env.NEXT_PUBLIC_SITE_DESCRIPTION || 'Komikcast adalah situs baca komik, baca manga, baca manhua, dan baca manhwa terpopuler dalam Bahasa Indonesia..',
+  keywords: (process.env.NEXT_PUBLIC_SITE_KEYWORDS || 'komikcast, komik online, komikcast to, komiku, manga, manhwa').split(', '),
   
   socials: {
     facebook: process.env.NEXT_PUBLIC_SOCIAL_FACEBOOK || '#',
